c5ed011dc17d22d516b38e81aa0d5b3c91b1d5c6
Kirill Byvshev <k@byv.sh>
2025-02-03 00:33:38 +0400

    Unchecked selfsigned certs ctl requests

--- omgserversctl.sh
+++ omgserversctl.sh
@@ -6,5 +6,6 @@ docker run --rm -it \
   --network=host \
   -v ${PWD}/.omgserversctl:/opt/omgserversctl/.omgserversctl \
   -v ${PWD}/config.json:/opt/omgserversctl/config.json \
+  -v ${PWD}/.curlrc:/root/.curlrc \
   omgservers/ctl:${OMGSERVERS_VERSION} $@
 
