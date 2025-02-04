40336ada8a9f16168b977c914c9bd3dfa221319b
Kirill Byvshev <k@byv.sh>
2025-02-05 00:16:41 +0400

    Using host network to connect to local environment

--- omgserversctl.sh
+++ omgserversctl.sh
@@ -3,6 +3,7 @@ set -e
 source omglocaltestingctl.env
 
 docker run --rm \
+  --network=host \
   -v ${PWD}/.omgserversctl:/opt/omgserversctl/.omgserversctl \
   -v ${PWD}/config.json:/opt/omgserversctl/config.json:ro \
   -v ${PWD}/.curlrc:/root/.curlrc:ro \
