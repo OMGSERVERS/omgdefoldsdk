daca6f65be0368106147957ff2bf767ca9fd95c5
Kirill Byvshev <k@byv.sh>
2025-02-05 01:25:40 +0400

    Interchanging with untrusted server certificates is no longer allowed

--- omgserversctl.sh
+++ omgserversctl.sh
@@ -6,6 +6,5 @@ docker run --rm \
   --network=host \
   -v ${PWD}/.omgserversctl:/opt/omgserversctl/.omgserversctl \
   -v ${PWD}/config.json:/opt/omgserversctl/config.json:ro \
-  -v ${PWD}/.curlrc:/root/.curlrc:ro \
   -v /etc/resolv.conf:/etc/resolv.conf:ro \
   omgservers/ctl:${OMGSERVERS_VERSION} $@
